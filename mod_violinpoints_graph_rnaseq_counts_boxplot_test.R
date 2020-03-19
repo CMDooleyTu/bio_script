4c37678130bc80d748dc8e3ec89bb4c3d9182197
Chris Dooley <christopher.m.dooley@gmail.com>
2020-03-19 21:12:27 +0100

    Update mod_violinpoints_graph_rnaseq_counts_boxplot_test.R

--- mod_violinpoints_graph_rnaseq_counts_boxplot_test.R
+++ mod_violinpoints_graph_rnaseq_counts_boxplot_test.R
@@ -101,8 +101,9 @@ if (grepl("violin", plotStyle)) {
         p <- ggplot(counts[counts$id == i,],
                aes(x=condition, y=count, color=condition)) +
             #geom_violin() + 
-            stat_summary(fun.data=data_summary, shape=3, size=1, color="black") +
+            #stat_summary(fun.data=data_summary, shape=3, size=1, color="black") +
             geom_boxplot(width=1, outlier.shape=NA, weight=2) +
+            stat_summary(fun.data=data_summary, shape=3, size=1, color="black") +
             #geom_crossbar(stat="summary", fun.y=data_summary, fun.ymax=max, fun.ymin=min) +
             #stat_summary(fun.data = mean_se, geom = "errorbar") +
             labs(x="condition", y="Normalised Counts", title=countData$name[i]) +
