910dd009f72e1827890385b850311f187e7032ce
Chris Dooley <christopher.m.dooley@gmail.com>
2020-03-20 07:04:25 +0100

    Update mod_violinpoints_graph_rnaseq_counts_boxplot_test.R

--- mod_violinpoints_graph_rnaseq_counts_boxplot_test.R
+++ mod_violinpoints_graph_rnaseq_counts_boxplot_test.R
@@ -108,6 +108,7 @@ if (grepl("violin", plotStyle)) {
             #stat_summary(fun.data = mean_se, geom = "errorbar") +
             labs(x="condition", y="Normalised Counts", title=countData$name[i]) +
             #theme_bw() +
+            theme_minimal() +
             theme(legend.position='right',
                   axis.ticks.x = element_blank(),
                   axis.text.x= element_blank(),
